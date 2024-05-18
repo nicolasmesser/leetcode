b3328fbf699022fcb42691b1b05701964526af7d
Nicolas Messer <nme@nicolasmesser.ch>
2024-05-18 11:41:44 +0200

    Updated First Challenge

--- 30-Days-of-JavaScript/createHelloWorld.js
+++ 30-Days-of-JavaScript/createHelloWorld.js
@@ -1,3 +1,6 @@
+// Create Hello World Function
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
 var createHelloWorld = function() {
     return function() {
         return "Hello World";
